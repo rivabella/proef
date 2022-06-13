2ad67bf88be53f058a1500ed5ddf793d777e22bd
Riva <rivahoen79@hotmail.com>
2022-06-13 12:14:06 +0200

    h 42/43 variabelen string integer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 
-alert("this is an alert");
-
-console.log("hallo dit is een log");
+var hallo = "Wat een mooie leeftijd";
+var leeftijd = 42;
+var exact = 42.9;
+alert(leeftijd + " is mijn leeftijd nu");
+leeftijd = leeftijd + 1;
+console.log(leeftijd + "en dit volgend jaar");
+leeftijd++;
+console.log(leeftijd + "en dit het jaar erna");
