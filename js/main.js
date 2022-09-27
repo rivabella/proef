95815b425927df53d81f514627d235fb020d0208
Riva <rivahoen79@hotmail.com>
2022-09-27 13:16:56 +0200

    oefenen

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
 var hallo = "Wat een mooie leeftijd";
+var riva = "hopla";
+const alles = ['water','brood', 'piza'];
+console.log(alles.join('-'));
 var leeftijd = 42;
 var exact = 42.9;
 alert(leeftijd + " is mijn leeftijd nu");
@@ -7,3 +10,7 @@ leeftijd = leeftijd + 1;
 console.log(leeftijd + "en dit volgend jaar");
 leeftijd++;
 console.log(leeftijd + "en dit het jaar erna");
+var hopla = "zomer";
+alert(hopla + "fijne zomer");
+
+alert(hallo);
