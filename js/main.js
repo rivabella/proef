ceca316158986d1f6ee27fb80255968d49b057df
Riva <rivahoen79@hotmail.com>
2022-09-29 01:18:13 +0200

    H47 for loop

--- js/main.js
+++ js/main.js
@@ -14,22 +14,25 @@
 // alert(hopla + "fijne zomer");
 //
 // alert(hallo);
-var pablo = true;
-
-if(pablo==true){
-  console.log("zo zit dat")
-}else{
-  console.log("zo zit dat niet")
-}
-
-var leeftijd =28;
-
-if(leeftijd<3){
-  console.log("je bent een baby")
-}else if(leeftijd<10){
-  console.log("je bent bijna een tiener")
-}else if(leeftijd>=18){
-  console.log("je bent een volwassene ")
-}else{
-  console.log("je bent niet in onze database")
+// var pablo = true;
+//
+// if(pablo==true){
+//   console.log("zo zit dat")
+// }else{
+//   console.log("zo zit dat niet")
+// }
+//
+// var leeftijd =28;
+//
+// if(leeftijd<3){
+//   console.log("je bent een baby")
+// }else if(leeftijd<10){
+//   console.log("je bent bijna een tiener")
+// }else if(leeftijd>=18){
+//   console.log("je bent een volwassene ")
+// }else{
+//   console.log("je bent niet in onze database")
+// }
+for(var optellen=10; optellen>4; optellen--){
+ console.log("het getal is nu", optellen);
 }
