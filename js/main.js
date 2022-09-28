854843ea5c89843951adeafce8c57af6279c0970
Riva <rivahoen79@hotmail.com>
2022-09-28 20:54:29 +0200

    H46 if else loop

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 
-var hallo = "Wat een mooie leeftijd";
-var riva = "hopla";
-const alles = ['water','brood', 'piza'];
-console.log(alles.join('-'));
-var leeftijd = 42;
-var exact = 42.9;
-alert(leeftijd + " is mijn leeftijd nu");
-leeftijd = leeftijd + 1;
-console.log(leeftijd + "en dit volgend jaar");
-leeftijd++;
-console.log(leeftijd + "en dit het jaar erna");
-var hopla = "zomer";
-alert(hopla + "fijne zomer");
+// var hallo = "Wat een mooie leeftijd";
+// var riva = "hopla";
+// const alles = ['water','brood', 'piza'];
+// console.log(alles.join('-'));
+// var leeftijd = 42;
+// var exact = 42.9;
+// alert(leeftijd + " is mijn leeftijd nu");
+// leeftijd = leeftijd + 1;
+// console.log(leeftijd + "en dit volgend jaar");
+// leeftijd++;
+// console.log(leeftijd + "en dit het jaar erna");
+// var hopla = "zomer";
+// alert(hopla + "fijne zomer");
+//
+// alert(hallo);
+var pablo = true;
 
-alert(hallo);
+if(pablo==true){
+  console.log("zo zit dat")
+}else{
+  console.log("zo zit dat niet")
+}
+
+var leeftijd =28;
+
+if(leeftijd<3){
+  console.log("je bent een baby")
+}else if(leeftijd<10){
+  console.log("je bent bijna een tiener")
+}else if(leeftijd>=18){
+  console.log("je bent een volwassene ")
+}else{
+  console.log("je bent niet in onze database")
+}
