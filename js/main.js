8948d8c7501f25dc64131d9b79ff77693645d570
Riva <rivahoen79@hotmail.com>
2022-10-04 23:51:03 +0200

    54. Textareas and getting the value of inputs

--- js/main.js
+++ js/main.js
@@ -37,42 +37,47 @@
 //  console.log("het getal is nu", optellen);
 // }
 var btn= document.getElementById("go");
+
 function buttonClicked(){
-  console.log("button clicked");
-  btn.removeEventListener("click", buttonClicked);
-  document.getElementById("peter").innerHTML="ik ben peter";
-  console.log(document.getElementById("peter").innerHTML);
+  // console.log("button clicked");
+  var my_answer= document.getElementsByClassName("info");
+  var text_output = document.getElementById("peter");
+  text_output.innerHTML= "Hallo " + my_answer[0].value;
+  // btn.removeEventListener("click", buttonClicked);
+  // document.getElementById("peter").innerHTML="ik ben peter";
+  // console.log(document.getElementById("peter").innerHTML);
 }
+
 btn.addEventListener("click",buttonClicked);
 
-var hobbies = ["voedbal", "tennis", "korfbal"];
-console.log("dit waren mijn hobbies",hobbies);
-
-console.log("deze gaat weg", hobbies.pop());
-hobbies.push("baskedbal");
-console.log("Dit zijn mijn hobbies nu", hobbies);
-console.log("dit is mijn favo", hobbies[hobbies.length-1]);
-hobbies.forEach(function(print,){
-  console.log("i like",print)
-});
-hobbies.shift();
-hobbies.forEach(function(print,){
-  console.log("i like",print)
-});
-hobbies.unshift("wielrennen")
-hobbies.forEach(function(print,){
-  console.log("i like",print)
-});
-var indexnum= hobbies.indexOf("baskedbal");
-console.log(indexnum);
-
-hobbies.splice(indexnum,1);
-console.log(hobbies);
-if(hobbies.indexOf("wielrennen")>-1){
-  console.log("wielrennen is in de array");
-}
-if(hobbies.indexOf("sport") > -1){
-
-}else{
-  console.log("Sport is niet in de array");
-}
+// var hobbies = ["voedbal", "tennis", "korfbal"];
+// console.log("dit waren mijn hobbies",hobbies);
+//
+// console.log("deze gaat weg", hobbies.pop());
+// hobbies.push("baskedbal");
+// console.log("Dit zijn mijn hobbies nu", hobbies);
+// console.log("dit is mijn favo", hobbies[hobbies.length-1]);
+// hobbies.forEach(function(print,){
+//   console.log("i like",print)
+// });
+// hobbies.shift();
+// hobbies.forEach(function(print,){
+//   console.log("i like",print)
+// });
+// hobbies.unshift("wielrennen")
+// hobbies.forEach(function(print,){
+//   console.log("i like",print)
+// });
+// var indexnum= hobbies.indexOf("baskedbal");
+// console.log(indexnum);
+//
+// hobbies.splice(indexnum,1);
+// console.log(hobbies);
+// if(hobbies.indexOf("wielrennen")>-1){
+//   console.log("wielrennen is in de array");
+// }
+// if(hobbies.indexOf("sport") > -1){
+//
+// }else{
+//   console.log("Sport is niet in de array");
+// }
