1c60c22138f125fcb0e09083ebc609e9f173ebb4
rivabella <rivahoen79@hotmail.com>
2022-12-02 18:06:15 +0100

    H66 drtopdown

--- js/main.js
+++ js/main.js
@@ -20,5 +20,13 @@ console.log("tekst is:", $(tekst);
 $('#go').on('click', function(){
   alert("piepo");
 })
+// $('[data-trigger="dropdown"]').on('mouseover', is de trigger dus als de muis erover
+// heen gaat wordt de functie aangeroepen
+$('[data-trigger="dropdown"]').on('mouseover',function(){
+  // variabele= a regel 21
+  var submenu = $(this).parent().find('.submenu');
+  // omdat er al een $ in de variabele zit hoeft dat niet hieronder
+  submenu.addClass('active');
+})
 
 })
